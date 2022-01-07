09c8857de4dafc8c72f1b76dad7d55512b2e9262
ingridarreola <ingrid.arreola47@gmail.com>
2022-01-07 18:42:26 -0500

    Adding in the final exercise for classes.

---
+++ Python Fundamentals/Class/Class_Exercise_Projcet.py
@@ -0,0 +1,15 @@
+# Intro to Classes ---> Python Classes: Medical Insurance Project
+
+### Objective: You have been asked to develop a system that makes it easier to organize patient data. You will create a class that does the following: 
+# - Takes in patient parameters regarding their personal information
+# - Contains methods that allow users to update their information
+# - Gives patients insight into their potential medical fees.
+
+## [Step 0]
+####### If you look at script.py, you will see that we have started a class called Patient. It currently has an __init__ method with two class variables: self.name and self.age. Let’s start by adding in some more patient parameters:
+### - sex: patient’s biological identification, 0 for male and 1 for female
+### - bmi: patient BMI
+### - num_of_children: number of children patient has
+### - smoker: patient smoking status, 0 for a non-smoker and 1 for a smoker
+# Add these into the __init__ method so that we can use them as we create our class methods.
+
